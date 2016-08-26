57c9a0699a7585618b753c1051ce14433e8bc5a0
Jackson Meyer <jackson.meyer7@gmail.com>
2016-08-26 16:58:45 -0700

    added more js, still struggling

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ deliveryAddress.prototype.WhereAreWeTakingThisPizzaThisTime = function() {
   return this.street + "Is nearby! I'll be there shortly!";
 };
 
-var totalprice = (sizeInput + toppingInput1);
+
 
 // UI Logic
 $(document).ready(function() {
@@ -27,9 +27,22 @@ $(document).ready(function() {
   var sizeInput=$("#size").val();
   var toppingInput1=$("#topping1").val();
   var addressInput1=$("#address1").val();
+
   var newOrder = new pizzaOrder(sizeInput, toppingInput1);
-    console.log(newOrder);
-  $(".price").text(newOrder.pizzaSize);
+  var changeToNumber = parseInt(sizeInput)
+  var changeToppingToAPrice = parseInt(toppingInput1);
+  var finalPrice = parseInt(toppingInput1) + parseInt(sizeInput);
+
+  // if (changeToNumber < 10) alert("please enter a larger pizza, we make big pizzas!") if ((totalSizePrice >= 11) && (totalSizePrice <= 30))  return 25. if ((totalSizePrice >= 31) && (totalSizePrice <= 50))  return 55. if ((totalSizePrice >) return 100
+  // }
+  //
+  // if (changeToppingToAPrice === 1) {return 2} if (changeToppingToAPrice === 2) {return 4 }if (changeToppingToAPrice === 3){ return 6 }if (changeToppingToAPrice === 1){ return 8} if (changeToppingToAPrice === 5){ return 10};
+
+
+  console.log(finalPrice);
+
+  $(".price").text(finalPrice);
+
   // $(".last-name").text(newContact.lastName);
 
 
@@ -49,22 +62,6 @@ $(document).ready(function() {
   //   $("input#new-last-name").val("");
 })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //
 //
 // if (input <= 9) {
